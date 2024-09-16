c8cc17faa9e63612f9663f3e3ac3f75424e3b9ca
Pamela Brickman <pamela.brickman@gmail.com>
2024-09-16 19:36:57 -0300

    Add functionality to grid-size input and reset button

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function createNewGrid(gridSize) {
     const grid = document.querySelector("#grid");
     for (let i = 0; i < gridSize; i++) {
-        const newGridHorizontalItem = document.createElement("div");
-        newGridHorizontalItem.classList.add("grid-col");
+        const newGridColumn = document.createElement("div");
+        newGridColumn.classList.add("grid-col");
         for (let j = 0; j < gridSize; j++) {
-            const newGridVerticalItem = document.createElement("div");
-            newGridVerticalItem.classList.add("grid-item");
-            newGridHorizontalItem.appendChild(newGridVerticalItem);
+            const newGridItem = document.createElement("div");
+            newGridItem.classList.add("grid-item");
+            newGridColumn.appendChild(newGridItem);
         };
-        grid.appendChild(newGridHorizontalItem);
+        grid.appendChild(newGridColumn);
     };
 };
+
+function resetGrid() {
+    grid.replaceChildren();
+}
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    const newGridSize = document.querySelector("#grid-size").value;
+    resetGrid();
+    createNewGrid(newGridSize);
+})
 
 createNewGrid(16);
