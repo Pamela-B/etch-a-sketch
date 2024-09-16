bb5a5bc68c59b1bd327b2827d2359ad05fdde399
Pamela Brickman <pamela.brickman@gmail.com>
2024-09-16 19:13:28 -0300

    Move styling of grid items to styles.css for cleaner code

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function createNewGrid(gridSize) {
     const grid = document.querySelector("#grid");
     for (let i = 0; i < gridSize; i++) {
         const newGridHorizontalItem = document.createElement("div");
-        newGridHorizontalItem.style.cssText = "flex: 1 1 auto; display: flex; flex-direction: column;";
+        newGridHorizontalItem.classList.add("grid-col");
         for (let j = 0; j < gridSize; j++) {
             const newGridVerticalItem = document.createElement("div");
-            newGridVerticalItem.style.cssText = "flex: 1 1 auto; border: 1px solid rgb(200 200 200);";
+            newGridVerticalItem.classList.add("grid-item");
             newGridHorizontalItem.appendChild(newGridVerticalItem);
         };
         grid.appendChild(newGridHorizontalItem);
